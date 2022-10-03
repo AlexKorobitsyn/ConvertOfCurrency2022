0a83e359957b0bdf905035563f4dcdf4e7cc540e
AlexKorobitsyn <A.E.Korobitsyn@urfu.me>
2022-10-03 21:04:02 +0500

    Test Convert

--- src/test/java/ConvertTest.java
+++ src/test/java/ConvertTest.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConvertTest {
-
+    @org.junit.jupiter.api.Test
+    void testConvertationMethod(){
+        HashMap<String, Double> dict = new HashMap<>();
+        dict.put("USD", 50.0);
+        dict.put("EUR", 70.0);
+        dict.put("RUB", 1.0);
+        Convert convert = new Convert(dict);
+        assertEquals(139930.0, Convert.convertation("EUR","RUB", 1999.0));
+        assertEquals(2000.0, Convert.convertation("RUB","RUB", 2000.0));
+        assertEquals(22500,Convert.convertation("USD","RUB", 450.0));
+    }
 }
