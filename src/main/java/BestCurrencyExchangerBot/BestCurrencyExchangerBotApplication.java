3d83471d06719bceb9ee248e85a0292b042f2a64
AlexKorobitsyn <A.E.Korobitsyn@urfu.me>
2022-11-22 21:53:37 +0500

    Start TelegramBot

---
+++ src/main/java/BestCurrencyExchangerBot/BestCurrencyExchangerBotApplication.java
@@ -0,0 +1,13 @@
+package BestCurrencyExchangerBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestCurrencyExchangerBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestCurrencyExchangerBotApplication.class, args);
+    }
+
+}
