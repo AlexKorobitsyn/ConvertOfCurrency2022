cee618ef6654fbf02ae2637e4c4725e4aadcb7be
AlexKorobitsyn <A.E.Korobitsyn@urfu.me>
2022-11-28 20:59:36 +0500

    added method with maps

--- src/main/java/BestCurrencyExchangerBot/service/TelegramBot.java
+++ src/main/java/BestCurrencyExchangerBot/service/TelegramBot.java
@@ -1,11 +1,12 @@
 package BestCurrencyExchangerBot.service;
 
-
 import BestCurrencyExchangerBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendLocation;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Location;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -48,23 +49,18 @@ public class TelegramBot extends TelegramLongPollingBot {
                     break;
 
                 default:
-
                     sendMessage(chatId, "Команда не распознана");
 
             }
         }
-
-
-
     }
 
     private void startCommandReceived(long chatId, String name){
 
-
         String answer = "Привет, " + name +", рады тебя видеть!";
         log.info("Replied to user: " + name);
-
         sendMessage(chatId, answer);
+        sendLocation(chatId,51.111, 41.1111 );
     }
 
     private void sendMessage(long chatId, String textToSend)  {
@@ -74,6 +70,15 @@ public class TelegramBot extends TelegramLongPollingBot {
 
         try{
             execute(message);
+        }
+        catch (TelegramApiException e) {
+            log.error("Error occurred:" + e.getMessage());
+        }
+    }
+    private void sendLocation(long chatId, double latitude, double longitude)  {
+        SendLocation location = new SendLocation(String.valueOf(chatId), latitude, longitude);
+        try{
+            execute(location);
         }
         catch (TelegramApiException e) {
             log.error("Error occurred:" + e.getMessage());
