30e144dd64ada1bd0315bd476a1f733423441e46
Akulin_Dmitry <newmitya2@gmail.com>
2022-12-01 18:26:15 +0500

    Improve Bot hierarchy

--- src/main/java/BestCurrencyExchangerBot/service/TelegramBot.java
+++ src/main/java/BestCurrencyExchangerBot/service/TelegramBot.java
@@ -1,5 +1,4 @@
 package BestCurrencyExchangerBot.service;
-
 import BestCurrencyExchangerBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -11,7 +10,6 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
 import java.util.ArrayList;
 
 @Slf4j
@@ -45,8 +43,9 @@ public class TelegramBot extends TelegramLongPollingBot {
             if (step == 1) {
                 switch (messageText) {
                     case "/start":
-//                        sendKeyboard(chatId, secondStepKeyboard());
+                        String text = "Введите город, в котором хотите найти обменник валют";
                         startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                        sendMessageWithKeyboard(chatId, text, secondStepKeyboard());
                         step = 2;
                         break;
                     case "/back":
@@ -55,7 +54,6 @@ public class TelegramBot extends TelegramLongPollingBot {
                     case "Mitya":
                         sendMessage(chatId, "Что делаешь " + update.getMessage().getChat().getFirstName()+"?");
                         break;
-
                     default:
                         sendMessage(chatId, "Команда не распознана");
                         break;
@@ -65,12 +63,13 @@ public class TelegramBot extends TelegramLongPollingBot {
                 switch (messageText) {
                     case "/back":
                         step = 1;
-                        sendMessage(chatId, "Возвращаюсь на шаг номер 1. (Введите /start)");
+                        sendMessageWithKeyboard(chatId,
+                                "Возвращаюсь на шаг номер 1. (Введите /start)", firstStepKeyboard());
                         break;
                     default:
                         sendMessage(chatId, "Запускаю поиск банков в выбранном городе");
                         sendMessage(chatId, "Список найденных банков с их курсами:");
-                        sendMessage(chatId, "Выберите Банк");
+                        sendMessageWithKeyboard(chatId, "Выберите Банк", thirdStepKeyboard());
                         step = 3;
                         break;
                 }
@@ -79,11 +78,12 @@ public class TelegramBot extends TelegramLongPollingBot {
                 switch (messageText) {
                     case "/back":
                         step = 2;
-                        sendMessage(chatId, "Возвращаюсь на шаг номер  2. (Выбор города)");
+                        sendMessageWithKeyboard(chatId,
+                                "Возвращаюсь на шаг номер  2. (Выбор города)", secondStepKeyboard());
                         break;
                     default:
-                        sendLocation(chatId, 51.111, 41.1111 );
-                        sendMessage(chatId, "Выберите, Calculate или Change");
+                        sendMessageWithKeyboard(chatId, "Выберите, Calculate или Change", fourthStepKeyboard());
+                        step = 4;
                         break;
                 }
             }
@@ -91,15 +91,18 @@ public class TelegramBot extends TelegramLongPollingBot {
                 switch (messageText) {
                     case "/back":
                         step = 3;
-                        sendMessage(chatId, "Возвращаюсь на шаг номер 3. (Выбор банка)");
+                        sendMessageWithKeyboard(chatId, "Возвращаюсь на шаг номер 3. (Выбор банка)", thirdStepKeyboard());
                         break;
                     case "Calculate":
-                        sendMessage(chatId, "Вы в режиме Калькулятора");
-                        step = 4;
+                        sendMessageWithKeyboard(chatId, "Вы в режиме Калькулятора", fifthStepKeyboard());
+                        step = 5;
                         break;
                     case "Change":
-                        sendMessage(chatId, "Вы в режиме Обмена");
-                        step = 4;
+                        sendMessageWithKeyboard(chatId, "Вы в режиме Обмена", fifthStepKeyboard());
+                        step = 5;
+                        break;
+                    case "Map":
+                        sendLocation(chatId, 51.111, 41.1111 );
                         break;
                     default:
                         sendMessage(chatId, "Команда не распознана");
@@ -109,40 +112,82 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
-//    private ArrayList<KeyboardRow> secondStepKeyboard() {
-//        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
-//        KeyboardRow row = new KeyboardRow();
-//        row.add("Екатеринбург");
-//        keyboardRows.add(row);
-//        row = new KeyboardRow();
-//        row.add("/back");
-//        keyboardRows.add(row);
-//        return keyboardRows;
-//    }
+    private ArrayList<KeyboardRow> firstStepKeyboard() {
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+        row.add("/start");
+        keyboardRows.add(row);
+        return keyboardRows;
+    }
+    private ArrayList<KeyboardRow> secondStepKeyboard() {
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+        row.add("Екатеринбург");
+        keyboardRows.add(row);
+        row = new KeyboardRow();
+        row.add("/back");
+        keyboardRows.add(row);
+        return keyboardRows;
+    }
+    private ArrayList<KeyboardRow> thirdStepKeyboard() {
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+        row.add("Сбербанк");
+        keyboardRows.add(row);
+        row = new KeyboardRow();
+        row.add("ВТБ");
+        keyboardRows.add(row);
+        row = new KeyboardRow();
+        row.add("/back");
+        keyboardRows.add(row);
+        return keyboardRows;
+    }
+    private ArrayList<KeyboardRow> fourthStepKeyboard() {
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+        row.add("Calculate");
+        keyboardRows.add(row);
+        row = new KeyboardRow();
+        row.add("Change");
+        keyboardRows.add(row);
+        row = new KeyboardRow();
+        row.add("Map");
+        keyboardRows.add(row);
+        row = new KeyboardRow();
+        row.add("/back");
+        keyboardRows.add(row);
+        return keyboardRows;
+    }
+
+    private ArrayList<KeyboardRow> fifthStepKeyboard() {
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+        row.add("some operation");
+        keyboardRows.add(row);
+        return keyboardRows;
+    }
 
     private void startCommandReceived(long chatId, String name){
 
-        String answer = "Привет, " + name +", рады тебя видеть!" +
-                "\nВведите город, в котором хотите найти обменник валют";
+        String answer = "Привет, " + name +", рады тебя видеть!\n";
         log.info("Replied to user: " + name);
         sendMessage(chatId, answer);
-//        sendLocation(chatId,51.111, 41.1111 );
     }
 
-//    private void sendKeyboard(long chatId, ArrayList<KeyboardRow> keyboardRows){
-//        SendMessage message = new SendMessage();
-//        message.setChatId(String.valueOf(chatId));
-//        message.setText("");
-//        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-//        keyboardMarkup.setKeyboard(keyboardRows);
-//        message.setReplyMarkup(keyboardMarkup);
-//        try{
-//            execute(message);
-//        }
-//        catch (TelegramApiException e) {
-//            log.error("Error occurred:" + e.getMessage());
-//        }
-//    }
+    private void sendMessageWithKeyboard(long chatId, String text, ArrayList<KeyboardRow> keyboardRows){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(text);
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setKeyboard(keyboardRows);
+        message.setReplyMarkup(keyboardMarkup);
+        try{
+            execute(message);
+        }
+        catch (TelegramApiException e) {
+            log.error("Error occurred:" + e.getMessage());
+        }
+    }
 
 
     private void sendMessage(long chatId, String textToSend)  {
